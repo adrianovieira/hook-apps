e91a8298dfa3d1e657036799a966a731042f8be5
Adriano dos Santos Vieira <adriano.vieira@dataprev.gov.br>
2014-11-15 02:37:01 -0200

    Melhoria em forma de verificar tópicos com while

--- webhook/artigo/verifica.py
+++ webhook/artigo/verifica.py
@@ -6,20 +6,26 @@ def topicosBase(file_name):
     introducao_header = False
 
     with open(file_name) as file:
-        for index, line in enumerate(file):
-            if (line.find('Introducao') <> -1) or (line.find('# Introducao') <> -1):
+        while True:
+            line = file.readline()
+            if not line:
+               break
+
+            if (line.find('Introdução') <> -1) or (line.find('# Introdução') <> -1):
                 introducao_header = True
-                print line
 
             if introducao_header:
-               print line[index+1]
-               if line[index+1].find('===') == 0:
+               line = file.readline()
+               if not line:
+                  break
+
+               if line.find('===') == 0:
                   introducao_found = True
 
     file.close()
 
     if introducao_found:
-       print "**Introducao**"
+       print "**Introducao**: encontrado"
     else: 
        print u"Topico **Introducao** não encontrado"
 
