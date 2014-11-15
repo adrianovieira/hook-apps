d267f67ac0eb753e62f514fbe4043578c99faf4a
Adriano dos Santos Vieira <adriano.vieira@dataprev.gov.br>
2014-11-15 02:52:31 -0200

    Verifica tópico "Desafios"

--- webhook/artigo/verifica.py
+++ webhook/artigo/verifica.py
@@ -5,22 +5,34 @@ def topicosBase(file_name):
     introducao_found = False
     introducao_header = False
 
+    desafios_found = False
+    desafios_header = False
+
     with open(file_name) as file:
         while True:
             line = file.readline()
             if not line:
                break
 
+            #  verifica tópico "Introdução"
             if (line.find('Introdução') <> -1) or (line.find('# Introdução') <> -1):
                 introducao_header = True
-
             if introducao_header:
                line = file.readline()
                if not line:
                   break
-
                if line.find('===') == 0:
                   introducao_found = True
+
+            #  verifica tópico "Desafios"
+            if (line.find('Desafios') <> -1) or (line.find('# Desafios') <> -1):
+                desafios_header = True
+            if desafios_header:
+               line = file.readline()
+               if not line:
+                  break
+               if line.find('===') == 0:
+                  desafios_found = True
 
     file.close()
 
@@ -28,4 +40,9 @@ def topicosBase(file_name):
        print "**Introducao**: encontrado"
     else: 
        print u"Topico **Introducao** não encontrado"
+
+    if desafios_found:
+       print "**Desafios**: encontrado"
+    else: 
+       print u"Topico **Desafios** não encontrado"
 
