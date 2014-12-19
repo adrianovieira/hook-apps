2b7244d98af16f989649c6d0c07e21ad42562ad7
Adriano dos Santos Vieira <adriano.vieira@dataprev.gov.br>
2014-12-19 09:29:01 -0200

    Altera lógica de teste para grupos de metadados
    - pelo menos dois campos de cada grupo de metadados devem existir
      - de autor: grupo e resumo
      - de referencias: grupo e id

--- webhook/artigo/verifica_metadados.py
+++ webhook/artigo/verifica_metadados.py
@@ -34,6 +34,11 @@ class VerificaMetadados:
         if (len(file_name) == 0) and (len(self.__file_name) > 0):
            file_name = self.__file_name
 
+        __has_autor_group = False
+        __has_autor_abstract = False
+        __has_referencias_group = False
+        __has_referencias_id = False
+
         with open(file_name) as file:
             while True:  # loop para ler conteudo do arquivo
                 line = file.readline()
@@ -41,12 +46,25 @@ class VerificaMetadados:
                    break
 
                 #  verifica metadados de autor
-                if (line.find('author:') == 0) or (line.find('abstract:') == 0):
-                   self.__has_dados_autor = True
+                if (line.find('author:') == 0):
+                   __has_autor_group = True
+
+                if (line.find('abstract:') == 0):
+                   __has_autor_abstract = True
 
                 #  verifica metadados de referencias
-                if (line.find('references:') == 0) or (line.find('- id:') == 0):
-                   self.__has_dados_referencias = True
+                if (line.find('references:') == 0):
+                   __has_referencias_group = True
+
+                if (line.find('- id:') == 0):
+                   __has_referencias_id = True
 
         file.close()
+
+        #  pelo menos dois campos de cada grupo de metadados devem existir
+        if (__has_autor_group) and (__has_autor_abstract):
+           self.__has_dados_autor= True
+        
+        if (__has_referencias_group) and (__has_referencias_id):
+           self.__has_dados_referencias = True
 
