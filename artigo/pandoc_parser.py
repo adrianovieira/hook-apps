aac6665c3e2bf88c45b751ccc993cfa02cdb2683
Adriano dos Santos Vieira <adriano.vieira@dataprev.gov.br>
2015-08-21 17:09:42 -0400

    logging de mensagem se houver erro ao comentar merge request
    - mostrar mensagens de logging somente se em modo debug

--- artigo/pandoc_parser.py
+++ artigo/pandoc_parser.py
@@ -25,12 +25,13 @@ class PandocParser:
 
     def __init__(self, app):
         if not isinstance(app.gitlab, gitlab.Gitlab):
-            app.logger.debug('PandocParser: argument gitlab object needed', 'APP_WEBHOOK', 'PCT_artigo', 'PandocParser')
+            if app.debug:
+                app.logger.debug('PandocParser: argument gitlab object needed', 'APP_WEBHOOK', 'PCT_artigo', 'PandocParser')
             raise AttributeError('PandocParser: argument gitlab object needed', 'APP_WEBHOOK', 'PCT_artigo', 'PandocParser')
 
         self.__app = app
         self.__debug = app.debug
-
+        self.__debug_level = app.setup['DEBUG_LEVEL']
         self.__target_project_id = ''
         self.__mergerequest_id = ''
         self.__app_artigo_path = ''
@@ -53,7 +54,8 @@ class PandocParser:
         __result = False
         __has_dados_parser = True
 
-        self.__app.logger.debug('APP_Artigo_PandocParser: \n artigo-path: %s\n artigo-nome: %s', __app_artigo_path, __app_artigo_name)
+        if self.__debug:
+            self.__app.logger.debug('APP_Artigo_PandocParser: \n artigo-path: %s\n artigo-nome: %s', __app_artigo_path, __app_artigo_name)
 
         if not self.__target_project_id:
             if __target_project_id: self.__target_project_id = __target_project_id
@@ -72,18 +74,28 @@ class PandocParser:
             else: __has_dados_parser = False
 
         if not __has_dados_parser:
-            self.__app.logger.debug('pandocParser: all parameters are needed', 'APP_WEBHOOK', 'PCT_artigo', 'pandocParser')
+            if self.__debug:
+                self.__app.logger.debug('pandocParser: all parameters are needed', 'APP_WEBHOOK', 'PCT_artigo', 'pandocParser')
             raise AttributeError('pandocParser: all parameters are needed', 'APP_WEBHOOK', 'PCT_artigo', 'pandocParser')
 
         __log_message = u'Iniciada conversão do artigo **%s** para ***PDF***!' % self.__app_artigo_name
-        self.__app.logger.warning(__log_message)
+        if self.__debug and self.__debug_level <= logging.INFO:
+            self.__app.logger.info("projID: %s | MR: %s | Msg: %s" % (self.__target_project_id, self.__mergerequest_id, __log_message))
 
         # insere comentário no merge request
         try:
-            self.__app.gitlab.addcommenttomergerequest(self.__target_project_id, \
-                                        self.__mergerequest_id, __log_message)
+            ok = self.__app.gitlab.addcommenttomergerequest(self.__target_project_id,
+                                            self.__mergerequest_id, __log_message)
+            if not ok:
+                if self.__debug:
+                    __log_message = u'projID: %s | MR: %s | Msg: %s' % \
+                            (self.__target_project_id, self.__mergerequest_id, \
+                             u'Erro ao tentar comentar no MR')
+                    self.__app.logger.error(__log_message)
+                    raise EnvironmentError('pandocParser: %s'%__log_message, 'APP_WEBHOOK', 'PCT_artigo', 'pandocParser')
         except Exception as erro:
-            self.__app.logger.warning(erro)
+            if self.__debug:
+                self.__app.logger.error(erro.args)
 
         return __result
         # end pandocParser
